eeb3eb71aa7bb46aa08bd95860f5439703389cfb
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-05-18 15:25:57 +0700

    add method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    private static int addNumbers(int a, int b){
+        return a + b;
+    }
+
+    private static int multiply(int a, int b){
+        return a + b;
+    }
+
+    private static double multiply(double a, double b){
+        return a + b;
+    }
 }
